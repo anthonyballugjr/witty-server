f21b96c5d2bbb59afac2890838693a3d09bda169
earmix <earmix@outlook.com>
2018-08-09 02:16:25 +0800

    created seeder

--- src/services/seeder/index.js
+++ src/services/seeder/index.js
@@ -1,7 +1,8 @@
 var fs = require('fs');
 var path = require('path');
 
-var Categories = require('./seed.categories');
+// var Categories = require('./seed.categories');
+var Categories = require('../../api/categories/categories.model');
 
 function readStream(dir, file) {
   dir = (dir !== 'default') ? dir : '../../public/csv/';
@@ -15,9 +16,24 @@ function readStream(dir, file) {
   }
 }
 
+function seedJSON(document, data) {
+  document.find().remove()
+    .then(() => {
+      document.create(data)
+        .then(() => {
+          console.log('Successfuly Seeded!');
+        }).catch(err => {
+          console.log(err);
+        });
+    }).catch(err => {
+      console.log(err);
+    });
+}
+
 module.exports = function (seedIfNeeded) {
-  var categoriesCSV = readStream('default', 'categoriesCSV.csv');
+  // var categoriesCSV = readStream('default', 'categories.csv');
   if (seedIfNeeded) {
-    Categories(categoriesCSV);
+    // Categories(categoriesCSV);
+    seedJSON(Categories, require('./seed.categoriesJSON'))
   }
 };
--- src/services/seeder/seed.categoriesJSON.js
+++ src/services/seeder/seed.categoriesJSON.js
@@ -1,97 +1,80 @@
-var Categories = require('../models/categories'); //use models/categories
-
 var categoryEntry = [
-    {
-        name: 'Food & Beverages',
-        budget: 5000,
-        expense: [
-            {
-                desc: 'groceries',
-                amount: 1300,
-                date: new Date()
-            },
-            {
-                desc: 'dined at mcdo',
-                amount: 200,
-                date: new Date()
-            },
-            {
-                desc: 'lunch',
-                amount: 40,
-                date: new Date()
-            }
+  {
+    name: 'Food & Beverages',
+    budget: 5000,
+    expense: [
+      {
+        desc: 'groceries',
+        amount: 1300,
+        date: new Date()
+      },
+      {
+        desc: 'dined at mcdo',
+        amount: 200,
+        date: new Date()
+      },
+      {
+        desc: 'lunch',
+        amount: 40,
+        date: new Date()
+      }
 
-        ]
-    },
-    {
-        name: 'Education',
-        budget: 4000,
-        expense: [
-            {
-                desc: 'tuition fee',
-                amount: 3000,
-                date: new Date()
-            },
-            {
-                desc: 'math book',
-                amount: 140,
-                date: new Date()
-            }
-        ]
-    },
-    {
-        name: 'Health Care & Fitness',
-        budget: 3000,
-        expense: [
-            {
-                desc: 'meds',
-                amount: 300,
-                date: new Date()
-            },
-            {
-                desc: 'checkup',
-                amount: 100,
-                date: new Date()
-            }
-        ]
-    },
-    {
-        name: 'Transportation',
-        budget: 1500,
-        expense: [
-            {
-                desc: 'maintenance',
-                amount: 500,
-                date: new Date()
-            }
-        ]
-    },
-    {
-        name: 'Entertainment',
-        budget: 2000,
-        expense:[
-            {
-                desc: 'movie night',
-                amount: 200,
-                date: new Date()
-            }
-        ]
-    }
+    ]
+  },
+  {
+    name: 'Education',
+    budget: 4000,
+    expense: [
+      {
+        desc: 'tuition fee',
+        amount: 3000,
+        date: new Date()
+      },
+      {
+        desc: 'math book',
+        amount: 140,
+        date: new Date()
+      }
+    ]
+  },
+  {
+    name: 'Health Care & Fitness',
+    budget: 3000,
+    expense: [
+      {
+        desc: 'meds',
+        amount: 300,
+        date: new Date()
+      },
+      {
+        desc: 'checkup',
+        amount: 100,
+        date: new Date()
+      }
+    ]
+  },
+  {
+    name: 'Transportation',
+    budget: 1500,
+    expense: [
+      {
+        desc: 'maintenance',
+        amount: 500,
+        date: new Date()
+      }
+    ]
+  },
+  {
+    name: 'Entertainment',
+    budget: 2000,
+    expense: [
+      {
+        desc: 'movie night',
+        amount: 200,
+        date: new Date()
+      }
+    ]
+  }
 ];
 
-module.exports = {
-    seed: function () {
-
-        Categories.find().remove()
-            .then(() => {
-                Categories.create(categoryEntry)
-                    .then(() => {
-                        console.log('Budget Seeded!');
-                    }).catch(err => {
-                        console.log(err);
-                    });
-            }).catch(err => {
-                console.log(err);
-            });
-    }
-};
+module.exports = categoryEntry;
