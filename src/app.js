ae6c8911fc8c0d611740a34b1abbfe263c61fb94
earmix <earmix@outlook.com>
2018-08-09 19:10:44 +0800

    cleanup dependencies: removed sass and hbs

--- src/app.js
+++ src/app.js
@@ -1,11 +1,9 @@
 var express = require('express');
 var path = require('path');
-// var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
-var sassMiddleware = require('node-sass-middleware');
 var mongoose = require('mongoose');
 var cors = require('cors');
 var routes = require('./routes');
@@ -18,26 +16,12 @@ app.use(cors());
 
 mongoose.promise = global.Promise;
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: false
 }));
 app.use(cookieParser());
-app.use(sassMiddleware({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  indentedSyntax: false, // true = .sass and false = .scss
-  sourceMap: true,
-  outputStyle: 'compressed'
-}));
-
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(session({
