ba0d7686c1c9d8334e5b7e5cb2d7512637105a8d
Anthony <anthonyballugjr@gmial.com>
2018-11-13 15:30:44 +0800

    .

--- src/api/arduino/controller.js
+++ src/api/arduino/controller.js
@@ -9,14 +9,11 @@ var controller = {
 
         if (trigger === 'on') {
             arduinoSwitch = 'on';
-            res.status(200).send({ arduino = 'on' });
+            res.status(200).send({ arduino: 'on' });
         } else {
             arduinoSwitch = 'off';
-            res.status(200).send({arduino: 'off'});
+            res.status(200).send({ arduino: 'off' });
         }
-
-        res.status(200).send({ count: data });
-        console.log(count);
     },
     feed: function (req, res) {
         res.send(counter);
