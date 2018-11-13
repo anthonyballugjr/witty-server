e74b52cff451f71b17a026259db3d86784883a7c
Anthony <anthonyballugjr@gmial.com>
2018-11-13 14:58:21 +0800

    ..

--- src/api/arduino/controller.js
+++ src/api/arduino/controller.js
@@ -1,13 +1,14 @@
-
+var data = 0;
 
 var controller = {
     home: function (req, res) {
-        res.status(200).send({ Message: 'Hello Reindel', Test: 'Server Test', counter: counter })
+        res.status(200).send({ Message: 'Hello Reindel', Test: 'Server Test', data: data })
     },
     postData: function (req, res) {
         var count = req.body.count;
+        data = count;
 
-        res.status(200).send({count: count});
+        res.status(200).send({count: data});
         counter = 0;
         console.log(count);
     },
