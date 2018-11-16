a9174796a1c58b4e319e00a4a3d1d171ae2f0e38
Anthony <anthonyballugjr@gmial.com>
2018-11-16 20:49:53 +0800

    .

--- src/public/json/categories.js
+++ src/public/json/categories.js
@@ -1,59 +1,18 @@
 var categoryEntry = [
-  {
-    _id: 'fnb',
-    desc: 'food',
-    icon: 'pizza'
-  },
-  {
-    _id: 'hc',
-    desc: 'health care',
-    icon: 'medkit'
-  },
-  {
-    _id: 'tra',
-    desc: 'transportation',
-    icon: 'bus'
-  },
-  {
-    _id: 'pc',
-    desc: 'personal care',
-    icon: 'person'
-  },
-  {
-    _id: 'edu',
-    desc: 'education',
-    icon: 'school'
-  },
-  {
-    _id: 'ent',
-    desc: 'entertainment',
-    icon: 'game-controller-b'
-  },
-  {
-    _id: 'lei',
-    desc: 'leisure',
-    icon: 'wine'
-  },
-  {
-    _id: 'trv',
-    desc: 'travel',
-    icon: 'plane'
-  },
-  {
-    _id: 'shp',
-    desc: 'shopping',
-    icon: 'pricetags'
-  },
-  {
-    _id: 'bll',
-    desc: 'bills',
-    icon: 'calendar'
-  },
-  {
-    _id: 'oth',
-    desc: 'others',
-    icon: 'clipboard'
-  }
+  { _id: 'fnb', desc: 'food', icon: 'pizza' },
+    { _id: 'edu', desc: 'education', icon: 'school' },
+    { _id: 'hse', desc: 'housing', icon: 'home' },
+    { _id: 'bll', desc: 'bills & utilities', icon: 'calendar' },
+    { _id: 'tra', desc: 'transportation', icon: 'bus' },
+    { _id: 'hc', desc: 'health and fitness', icon: 'medkit' },
+    { _id: 'ent', desc: 'entertainment', icon: 'game-controller-b' },
+    { _id: 'lei', desc: 'leisure', icon: 'wine' },
+    { _id: 'trv', desc: 'travel', icon: 'plane' },
+    { _id: 'shp', desc: 'shopping', icon: 'pricetags' },
+    { _id: 'dbt', desc: 'debts & loans', icon: 'card' },
+    {_id: 'gft', desc: 'gifts & donations', icon: 'heart' },
+    { _id: 'tax', desc: 'taxes', icon: 'paper' },
+    { _id: 'oth', desc: 'others', icon: 'clipboard' }
 ];
 
 module.exports = categoryEntry;
